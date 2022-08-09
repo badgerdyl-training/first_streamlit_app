6dcfee481b43e091d1126519bcf258babcf2a657
Dylan Badger <110920689+badgerdyl-training@users.noreply.github.com>
2022-08-09 15:31:47 -0400

    Update streamlit_app.py
    add requests packages
    add fruitvice response to the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header('Breakfast Favorites')
@@ -19,6 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display fruit table on page
 streamlit.dataframe(fruits_to_show)
+
+fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
 
